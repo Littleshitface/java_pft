bb6505dc75bae97bf69740bab98146f389142891
Littleshitface <103191013+Littleshitface@users.noreply.github.com>
2022-08-17 09:57:21 +0300

    Добавлен метод в класс point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,5 +7,9 @@ public class Point {
   public Point(double x, double y){
     this.x = x;
     this.y = y;
+
+}
+  public  double distance (Point p2){
+    return Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
   }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Test.java
@@ -4,10 +4,10 @@ public class Test {
   public static void main(String[] args){
     Point p1 = new Point(5, 4);
     Point p2 = new Point(9, 9);
-    System.out.println("Расстояние между точкой A и B = " + distance(p1, p2));
+    System.out.println("Расстояние между точкой A и B = " + p1.distance(p2));
   }
 
-  public static double distance (Point p1, Point p2){
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-  }
+  //public static double distance (Point p1, Point p2){
+   // return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  //}
 }
